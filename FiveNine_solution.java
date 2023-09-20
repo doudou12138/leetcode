9236d3b0be4f6868ea869447b74e8c31d914c238
doudou <211250173@smail.nju.edu.cn>
2023-09-20 12:28:09 +0800

    55,59

--- FiveNine_solution.java
+++ FiveNine_solution.java
@@ -1,8 +1,31 @@
-public class FiveNine_solution {
-    public int[][] generateMatrix(int n) {
-        int[][] result = new int[n][n];
+import java.util.Arrays;
 
-        
+public class FiveNine_solution {
+    public static int[][] generateMatrix(int n) {
+        int[][] result = new int[n][n];
+        int init = 1;
+        for(int i=0;i<(n+1)/2;++i){
+
+            for(int j=i;j<n-i;++j){
+                result[i][j]=init++;
+            }
+
+            for(int j=i+1;j<n-i;++j){
+                result[j][n-1-i]=init++;
+            }
+
+            for(int j=n-i-2;j>=i;--j){
+                result[n-i-1][j]=init++;
+            }
+
+            for(int j=n-i-2;j>i;--j){
+                result[j][i]=init++;
+            }
+
+        }
+
+        return result;
 
     }
+
 }
